c5ffaf433279766bf1327c5cd009c4eaf9baff8f
strixcuriosus <strixcuriosus@gmail.com>
2014-01-12 23:14:38 -0800

    used .charAt instead of char by index in firstReverse

--- firstReverse.js
+++ firstReverse.js
@@ -1,7 +1,7 @@
 function firstReverse(input) { 
   var str = '';
-  for ( var i = input.length - 1; i >=0; i--){
-    str += input[i];
+  for (var i = input.length - 1; i >=0; i--){
+    str += input.charAt(i);
   };
   return str;
 }

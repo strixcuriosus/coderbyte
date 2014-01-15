a0414be8ac35b84d5e43bc8bac12988f9b2b3573
strixcuriosus <strixcuriosus@gmail.com>
2014-01-14 23:48:26 -0800

    add letterCapitalize

---
+++ letterCapitalize.js
@@ -0,0 +1,9 @@
+function LetterCapitalize(str) { 
+  var arr = str.split(" ");
+  for (var i in arr){
+  arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  var ans = arr.join(" ")
+  return ans; 
+         
+}
